46b0b871306858d2c74ad43a6e39d174207aa455
Matveyx25 <matveyx25@gmail.com>
2021-12-24 00:22:48 +0400

    3 version

--- mixer/script.js
+++ mixer/script.js
@@ -25,14 +25,34 @@ const getNames = () => {
     document.querySelector('.count-value').innerHTML = namesArr.length
 }
 
+document.getElementById('area').addEventListener('input', () => {
+    if(!document.getElementById('area').value){
+        namesArr = []
+    }
+})
+
 const generateTeam = () => {
     let countInput = document.getElementById('countTeam')
     let out = document.querySelector('.out')
-    let countTeam = Math.ceil(namesArr.length / +countInput.value)
+    let countTeam = Math.floor(namesArr.length / +countInput.value)
     namesArr.sort(() => Math.random() - 0.5)
     let newArr = colSplit(namesArr , countTeam)
     for(let i = 0; i < newArr.length; i++) {
         newArr[i] = newArr[i].join(', ')
     }
+    if(newArr.length%2 != 0) {
+        const ln = newArr.length
+        newArr[ln - 1] = newArr[ln - 1] + ', '
+        newArr[ln - 2] = newArr[ln - 1].concat(newArr[ln - 2])
+        newArr.pop()
+        // newArr[ln - 1].join(', ')
+    }
+
     out.innerHTML = newArr.join(" | ")
 }
+
+const copy = () => {
+    var copyText = document.querySelector('.out')
+    navigator.clipboard.writeText(copyText.innerHTML);
+}
+
